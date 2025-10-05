b1ffcd1edb61f3f36698e5073944a387bfb7b55e
Juan David Fajardo Betancourt <jfajardob@unal.edu.co>
2025-10-04 20:12:16 -0500

    creacion de modelos rutas y configuraciones de notificaciones

--- Microservice_Notifications/settings.py
+++ Microservice_Notifications/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notifications',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +76,12 @@ WSGI_APPLICATION = 'Microservice_Notifications.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'microservice_notifications',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -120,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Notification
+from .serializer import NotificationSerializer
 
-# Create your views here.
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        """
+        Permite filtrar las notificaciones por user_id si se pasa como parámetro.
+        Ejemplo: GET /api/notifications/?user_id=5
+        """
+        user_id = self.request.query_params.get("user_id")
+        if user_id:
+            return Notification.objects.filter(user_id=user_id)
+        return Notification.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        """
+        Crea una nueva notificación.
+        Espera un JSON con los campos: user_id, title, message, type.
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
